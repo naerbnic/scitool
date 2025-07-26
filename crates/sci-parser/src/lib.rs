af4f397857c136b716b0080700e83b3765f10795
Brian Chin <brian.chin@gmail.com>
2025-07-26 16:53:51 -0700

    Delete old parser module, which compiles a subset of SCI script

--- crates/sci-parser/src/lib.rs
+++
@@ -1,5 +0,0 @@
-pub mod inputs;
-pub mod lexer;
-pub mod sci_script;
-pub mod sexpr;
-pub mod tokens;
--- crates/sci-parser/src/sci_script/parser.rs
+++
@@ -1,8 +0,0 @@
-use std::fmt::Error;
-
-use crate::sexpr::{SExpr, parse_funcs::SExprInput};
-
-#[expect(dead_code)]
-pub fn parse_script(_input: SExprInput<'_>) -> Result<Vec<SExpr>, Error> {
-    todo!()
-}
