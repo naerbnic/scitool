3448df9ac617895ee08ebda03650263cfcad28e7
Brian Chin <brian.chin@gmail.com>
2025-07-30 21:43:33 -0700

    Remove old JSON generation, as now we have proper serialization of the Book format

--- crates/scitool-cli/src/cli/generate.rs
+++ crates/scitool-cli/src/cli/generate.rs
@@ -1,9 +1,9 @@
-use std::{io::Write, path::PathBuf};
+use std::path::PathBuf;
 
 use clap::{Parser, Subcommand};
 use scidev_book::Book;
 
-use crate::generate::{csv::generate_csv, json::GameScript};
+use crate::generate::csv::generate_csv;
 
 #[derive(Parser)]
 struct CommonArgs {
@@ -16,43 +16,6 @@ fn load_book(args: &CommonArgs) -> anyhow::Result<Book> {
         &mut serde_json::Deserializer::from_reader(std::fs::File::open(&args.book_path)?),
     )?;
     Ok(book)
-}
-
-/// Generates a JSON representation of the game script.
-#[derive(Parser)]
-struct GenerateJson {
-    /// Path to the game's root directory.
-    #[clap(flatten)]
-    ctxt: CommonArgs,
-    /// Path to write the JSON output file.
-    #[clap(short, long)]
-    output: PathBuf,
-}
-
-impl GenerateJson {
-    fn run(&self) -> anyhow::Result<()> {
-        let book = load_book(&self.ctxt)?;
-        let script = GameScript::from_book(&book);
-        let json = serde_json::to_string(&script)?;
-        std::fs::write(&self.output, json)?;
-        Ok(())
-    }
-}
-
-/// Generates the JSON schema for the game script structure and prints it to stdout.
-#[derive(Parser)]
-struct GenerateJsonSchema;
-
-impl GenerateJsonSchema {
-    #[expect(
-        clippy::unused_self,
-        reason = "Future-proofing for potential arguments"
-    )]
-    fn run(&self) -> anyhow::Result<()> {
-        let schema = GameScript::json_schema()?;
-        std::io::stdout().write_all(schema.as_bytes())?;
-        Ok(())
-    }
 }
 
 #[derive(Parser)]
@@ -77,13 +40,6 @@ impl GenerateCsv {
 /// The specific generation command to execute.
 #[derive(Subcommand)]
 enum GenerateCommand {
-    #[clap(about = "Generates a JSON representation of the game script.")]
-    Json(GenerateJson),
-    #[clap(
-        name = "json-schema",
-        about = "Generates the JSON schema for the game script structure and prints it to stdout."
-    )]
-    JsonSchema(GenerateJsonSchema),
     #[clap(about = "Generates a CSV representation of the game script.")]
     Csv(GenerateCsv),
 }
@@ -99,8 +55,6 @@ pub(crate) struct Generate {
 impl Generate {
     pub(crate) fn run(&self) -> anyhow::Result<()> {
         match &self.msg_cmd {
-            GenerateCommand::Json(cmd) => cmd.run(),
-            GenerateCommand::JsonSchema(cmd) => cmd.run(),
             GenerateCommand::Csv(cmd) => cmd.run(),
         }
     }
--- crates/scitool-cli/src/generate.rs
+++ crates/scitool-cli/src/generate.rs
@@ -1,5 +1,3 @@
 //! Contains code used to generate outputs from scitool.
 
 pub(crate) mod csv;
-pub(crate) mod json;
-pub(crate) mod text;
