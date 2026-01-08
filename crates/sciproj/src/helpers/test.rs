61287194e73480349c4dd5c4aa6bb5a158a0fa9a
Brian Chin <brian.chin@gmail.com>
2026-01-07 22:05:45 -0800

    Add build_files helper to be able to write test files.

--- crates/sciproj/src/helpers/test.rs
+++ crates/sciproj/src/helpers/test.rs
@@ -30,5 +30,86 @@ macro_rules! from_json {
     };
 }
 
-pub(crate) use assert_matches;
-pub(crate) use from_json;
+macro_rules! make_file {
+    ($base_path:expr, $path:literal) => {
+        // Create an empty file at the location.
+        let path = $base_path.join($path);
+        std::fs::create_dir_all(path.parent().unwrap()).unwrap();
+        std::fs::write(path, "").unwrap();
+    };
+
+    ($base_path:expr, $path:literal => $contents:literal) => {
+        // Create a file at the location with the given contents.
+        let path = $base_path.join($path);
+        std::fs::create_dir_all(path.parent().unwrap()).unwrap();
+        std::fs::write(path, $contents).unwrap();
+    };
+
+    ($base_path:expr, $path:literal => {
+        $($dir_contents:tt)*
+    }) => {
+        let new_base_path = $base_path.join($path);
+        std::fs::create_dir_all(new_base_path.parent().unwrap()).unwrap();
+        $crate::helpers::test::make_files!(new_base_path, $($dir_contents)*);
+    };
+}
+
+macro_rules! make_files {
+    ($root_path:expr, $($path:literal $(=> $contents:tt)?),* $(,)?) => {
+        $(
+            $crate::helpers::test::make_file!($root_path, $path $( => $contents)?);
+        )*
+    };
+}
+
+macro_rules! build_files {
+    ($($path:literal $(=> $contents:tt)?),* $(,)?) => {{
+        let root = tempfile::TempDir::new().unwrap();
+        $(
+            $crate::helpers::test::make_files!(root.path(), $path $( => $contents)?);
+        )*
+        root
+    }};
+}
+
+pub(crate) use {assert_matches, from_json};
+#[expect(unused_imports)]
+pub(crate) use {build_files, make_file, make_files};
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_build_files_for_single_file() {
+        let root = build_files!("test.txt" => "test");
+        assert_eq!(
+            std::fs::read_to_string(root.path().join("test.txt")).unwrap(),
+            "test"
+        );
+    }
+
+    #[test]
+    fn test_build_files_for_directory() {
+        let root = build_files!("test_dir" => {
+            "test.txt" => "test",
+        });
+        assert!(root.path().join("test_dir/test.txt").exists());
+    }
+
+    #[test]
+    fn test_build_files_for_nested_directory() {
+        let root = build_files!("d1/d2" => {
+            "test.txt" => "test",
+        });
+        assert!(root.path().join("d1/d2/test.txt").exists());
+    }
+
+    #[test]
+    fn test_build_files_for_empty_file() {
+        let root = build_files!("empty.txt");
+        assert!(
+            std::fs::read_to_string(root.path().join("empty.txt"))
+                .unwrap()
+                .is_empty()
+        );
+    }
+}
